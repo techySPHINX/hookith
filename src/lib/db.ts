fc5049c4f8a4cd45e64ec7bc029aa131f78ec34f
Jagan Kumar Hotta <138506773+techySPHINX@users.noreply.github.com>
2025-04-18 23:09:21 +0530

    configure 👍 types

---
+++ src/lib/db.ts
@@ -0,0 +1,9 @@
+import { PrismaClient } from "@prisma/client";
+
+declare global {
+  let prisma: PrismaClient | undefined;
+}
+
+export const db = prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") prisma = db;
