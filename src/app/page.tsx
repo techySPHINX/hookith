7ef8390e4618edc68f11e2c10841382e5932f445
Jagan Kumar Hotta <138506773+techySPHINX@users.noreply.github.com>
2025-04-29 21:42:56 +0530

    add : authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { CardBody, CardContainer, CardItem } from "@/components/global/3d-card";
+import { HeroParallax } from "@/components/global/connect-parallax";
 import { ContainerScroll } from "@/components/global/container-scroll-animation";
 import { InfiniteMovingCards } from "@/components/global/infinite-moving-cards";
 import { LampComponent } from "@/components/global/lamp";
